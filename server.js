5a97e6ad3cb6e38e532b676116d2fd4dfc5ae31a
Hanzo <arta281@gmail.com>
2018-02-24 21:48:45 +0330

    Add project page

--- server.js
+++ server.js
@@ -52,6 +52,12 @@ app.get('/about',(req,res)=>{
   res.render('about.hbs',{
     pageTitle: 'About Page',
   });
+});
+
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle: 'Projects'
+  });
 
 });
 
